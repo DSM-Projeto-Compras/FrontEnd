6ee70960e5457220d3afe5c4ee933b085dc87d99
Thiago Diegoli <thiago_diegoli@hotmail.com>
2024-11-04 16:06:47 -0300

    feat: implement react admin dashboard page

---
+++ app/pages/admin-dashboard/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import AdminDashboardTemplate from "../../components/templates/admin-dashboard/AdminDashboardTemplate";
+
+const AdminDashboardPage: React.FC = () => <AdminDashboardTemplate />;
+
+export default AdminDashboardPage;
