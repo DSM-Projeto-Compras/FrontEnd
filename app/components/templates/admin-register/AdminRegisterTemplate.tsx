32b0f938c627280fbd0740456051237028dc8e0d
Felipe Thiago <felipethiagodasilva@gmail.com>
2025-10-03 17:29:55 -0300

    fix(registerAdmin): fix wrong text display and password input box

--- app/components/templates/admin-register/AdminRegisterTemplate.tsx
+++ app/components/templates/admin-register/AdminRegisterTemplate.tsx
@@ -8,7 +8,9 @@ import { useFormik } from "formik";
 const AdminRegisterTemplate: React.FC<{
     onRegister: (
         name: string,
-        email: string
+        email: string,
+        password: string,
+        confirmPassword: string
     ) => void;
     errorMessages?: string[];
 }> = ({ onRegister, errorMessages = [] }) => {
@@ -16,9 +18,11 @@ const AdminRegisterTemplate: React.FC<{
         initialValues: {
             name: "",
             email: "",
+            password: "",
+            confirmPassword: ""
         },
         onSubmit: (values) => {
-            onRegister(values.name, values.email);
+            onRegister(values.name, values.email, values.password, values.confirmPassword);
         },
         validationSchema: registerValidationSchema,
     });
@@ -93,13 +97,73 @@ const AdminRegisterTemplate: React.FC<{
                                         )}
                                     </div> {/* Fim Campo Email */}
 
+                                    <div>
+                                        <label
+                                            htmlFor="password"
+                                            className="block text-sm font-medium text-gray-900"
+                                        >
+                                            Senha
+                                        </label>
+                                        <input
+                                            type="password"
+                                            name="password"
+                                            id="password"
+                                            data-testid="password"
+                                            placeholder="••••••••"
+                                            value={formik.values.password}
+                                            onChange={formik.handleChange}
+                                            onBlur={formik.handleBlur}
+                                            className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${
+                                            formik.errors.password && formik.touched.password
+                                                ? "border-red-500"
+                                                : ""
+                                            }`}
+                                        />
+                                        {formik.touched.password && formik.errors.password && (
+                                            <p className="mt-1 text-sm text-red-600">
+                                            {formik.errors.password}
+                                            </p>
+                                        )}
+                                    </div>
+
+                                    <div>
+                                        <label
+                                            htmlFor="confirmPassword"
+                                            className="block text-sm font-medium text-gray-900"
+                                        >
+                                            Confirmar senha
+                                        </label>
+                                        <input
+                                            type="password"
+                                            name="confirmPassword"
+                                            id="confirmPassword"
+                                            data-testid="confirmPassword"
+                                            placeholder="••••••••"
+                                            value={formik.values.confirmPassword}
+                                            onChange={formik.handleChange}
+                                            onBlur={formik.handleBlur}
+                                            className={`bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5 ${
+                                            formik.errors.confirmPassword &&
+                                            formik.touched.confirmPassword
+                                                ? "border-red-500"
+                                                : ""
+                                            }`}
+                                        />
+                                        {formik.touched.confirmPassword &&
+                                        formik.errors.confirmPassword && (
+                                        <p className="mt-1 text-sm text-red-600">
+                                            {formik.errors.confirmPassword}
+                                        </p>
+                                        )}
+                                    </div>
+
                                     
                                     <button
                                         type="submit"
                                         className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                                         Criar Conta
                                     </button>
-                                    <p className="text-sm text-gray-400">Senha provisória gerada automaticamente e enviada para o email informado.</p>
+                                    {/* <p className="text-sm text-gray-400">Senha provisória gerada automaticamente e enviada para o email informado.</p> */}
                                     
                                 </form>
                             </div>
@@ -107,9 +171,6 @@ const AdminRegisterTemplate: React.FC<{
                     
                     </div>
                 </div>
-            </div>
-            <div>
-                <h1>Admin Register</h1>
             </div>
         </div>
     )
