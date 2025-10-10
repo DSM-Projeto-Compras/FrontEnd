68f25fad0ec6af5bdad9c6f5c996f9feab17aaa7
Felipe Thiago <felipethiagodasilva@gmail.com>
2025-10-10 17:13:29 -0300

    feat(admin): implement change admin password page

--- app/pages/change-password/page.tsx
+++ app/pages/change-password/page.tsx
@@ -1,7 +1,23 @@
-import ChangePasswordTemplate from "@/app/components/templates/change-password/ChangePasswordTemplate";
+"use client";
+
+import React, { useEffect } from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import { useRouter } from "next/navigation";
+import ChangePasswordAdminTemplate from "@/app/components/templates/change-password-admin/ChangePasswordAdminTemplate";
 
 const ChangePasswordPage: React.FC = () => {
-    return <ChangePasswordTemplate />;
-}
+  const { isAuthenticated, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !isAuthenticated) {
+      router.push("login");
+    }
+  }, [isAuthenticated, loading, router]);
+
+  if (loading) return null;
+
+  return isAuthenticated ? <ChangePasswordAdminTemplate /> : null;
+};
 
 export default ChangePasswordPage;
