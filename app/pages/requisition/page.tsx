44f5f56334c12c233df403cd879280ec893bc2ff
paulovictorio <paulocesarvictorio940@gmail.com>
2025-11-28 00:15:04 -0300

    feat/voice-recognition

--- app/pages/requisition/page.tsx
+++ app/pages/requisition/page.tsx
@@ -4,10 +4,13 @@ import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import RequisitionTemplate from "../../components/templates/requisition/RequisitionTemplate";
 import { useAuth } from "../../contexts/AuthContext";
+import { useVoice } from "@/app/contexts/VoiceContext";
 import ChatbotTemplate from "../../../app/components/templates/chat-bot/ChatbotTemplate";
+import VoiceTemplate from "@/app/components/templates/voice-recognition/VoiceTemplate";
 
 const RequisitionPage: React.FC = () => {
   const { isAuthenticated, loading } = useAuth();
+  const { isVoiceActive, toggleVoice, hasSpokenActivation, setHasSpokenActivation } = useVoice();
   const router = useRouter();
 
   useEffect(() => {
@@ -22,6 +25,12 @@ const RequisitionPage: React.FC = () => {
   <>
     <RequisitionTemplate />
     <ChatbotTemplate />
+    <VoiceTemplate 
+        isActive={isVoiceActive} 
+        onToggle={toggleVoice}
+        hasSpokenActivation={hasSpokenActivation}
+        setHasSpokenActivation={setHasSpokenActivation}
+    />
   </>
    : null;
 };
